67246537711e4038b4f6cec2df59149633d7ce2e
fatimaelasri01 <fatimaelassri041@gmail.com>
2023-09-18 18:14:03 -0700

    0x05. C - Pointers, arrays and strings tasks

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _puts - Print a string followed by a new line to stdout.
+ * @str: A pointer to a string (null-terminated character array).
+ */
+void _puts(char *str)
+{
+	if (str != NULL)
+	{
+		
+		while (*str != '\0')
+		{
+			_putchar(*str);
+			str++;
+		}
+		_putchar('\n');
+	}
+}
+
