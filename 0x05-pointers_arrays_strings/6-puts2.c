8a86aea7da56f492d1b86418e4c638032625c3c3
fatimaelasri01 <fatimaelassri041@gmail.com>
2023-09-19 15:05:38 -0700

    0x05. C - Pointers, arrays and strings tasks

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Prints every character
+ * @str: The input string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	
+	_putchar('\n');
+}
