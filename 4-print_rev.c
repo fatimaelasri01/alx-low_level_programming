3f7b9c80a046c6eef56dc970760eafc067e1f995
fatimaelasri01 <fatimaelassri041@gmail.com>
2023-09-18 18:30:10 -0700

    0x05. C - Pointers, arrays and strings tasks

---
+++ 4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+#include "2-strlen.c"
+
+/**
+ * print_rev - Print a string in reverse, followed by a new line.
+ * @s: A pointer to a string (null-terminated character array).
+ */
+void print_rev(char *s)
+{
+	int length = _strlen(s);
+	int i;
+	
+	if (s != NULL)
+	{
+		for (i = length - 1; i >= 0; i--)
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+	}
+}
