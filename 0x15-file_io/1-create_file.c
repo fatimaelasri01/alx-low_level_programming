a8403ddbcb9a18cac98637c7e45043eefa82deab
fatimaelasri01 <fatimaelassri041@gmail.com>
2023-11-03 11:46:52 -0700

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: file name.
+ * @text_content: null terminated string to be write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	ssize_t bytes = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		bytes = write(fd, text_content, len);
+	}
+	close(fd);
+
+	return ((bytes == len) ? 1 : -1);
+}
