c428979dbc09a0be5f64893983ae626b7130cca8
fatimaelasri01 <fatimaelassri041@gmail.com>
2023-12-11 16:40:12 -0800

    update

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,29 +1,71 @@
 #include "main.h"
 
-int add(int a, int b) {
-    return a + b;
+/**
+ * add - print the sum of two num
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
 }
 
-int sub(int a, int b) {
-    return a - b;
+/**
+ * sub - print sub of two num
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
 }
 
-int mul(int a, int b) {
-    return a * b;
+/**
+ * mul - print the multp  of two num
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the multp
+ */
+int mul(int a, int b)
+{
+	return (a * b);
 }
 
-int div(int a, int b) {
-    if (b == 0) {
-        printf("Error");
-        return 0;
-    }
-    return a / b;
+/**
+ * div - print the div of two num
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the div
+ */
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		return (0);
+	}
+	return (a / b);
 }
 
-int mod(int a, int b) {
-    if (b == 0) {
-        printf("Error");
-        return 0;
-    }
-    return a % b;
+/**
+ * mod - print mod of two num
+ * @a: the first num
+ * @b: the second num
+ *
+ * Return: the mod
+ */
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		return (0);
+	}
+	return (a % b);
 }
